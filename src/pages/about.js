86c7eaf6ed621dad85197422974dc45c680e4074
James Brocklebank <jgbrockl@uwaterloo.ca>
2022-01-14 12:08:01 -0500

    firs tcommit

---
+++ src/pages/about.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link, graphql } from 'gatsby';
+
+const About = ({ data }) => (
+    <React.Fragment>
+        <h2>{data.site.siteMetadata.title}</h2>
+        <div>{data.site.siteMetadata.description}</div>
+
+        <Link to="/">Back to home</Link>
+    </React.Fragment>
+)
+
+export default About;
+
+export const query = graphql `
+query {
+  site {
+    siteMetadata {
+      author
+      description
+      title
+    }
+  }
+}
+
+`;
